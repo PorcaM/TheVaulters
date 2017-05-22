c31648686f0e40a6da33b0e2c155fa6090560922
sj lee <tjdwns5122@gmail.com>
2017-05-22 22:33:55 +0900

    Init unit control
    Codes of Game play scene will be replaced to code of UnitControl.
    
    [ticket: X]

---
+++ src/unit_control.hpp
@@ -0,0 +1,21 @@
+/**
+	@file	unit_control.hpp
+	@date	2017/5/22
+	@author	¿Ãº∫¡ÿ
+	@brief
+*/
+
+#pragma once
+
+#include "unit.hpp"
+
+class UnitControl {
+private:
+
+public:
+	UnitControl() {}
+	void Init() {}
+	void Move();
+	void Jump();
+	void Vault();
+};
